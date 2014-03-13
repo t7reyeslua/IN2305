c79ffa23bb675889cd6ca235e384b7333185dcab
t7reyeslua <t7.reyeslua@gmail.com>
2014-03-13 15:47:10 +0100

    Changes after Day 2 *Included HW Decoder *Implemented interrupt_enable_register for debouncing (*for button_inc only) *Added Day3 functionality (3A)

--- cruise/cc.c
+++ cruise/cc.c
@@ -11,9 +11,12 @@ void    isr_buttons(void)
         OSIntEnter();
 
         switch (X32_BUTTONS){
-        	case 0x01: //INCREMENT THROTTLE
-        		OSSemPost(sem_button_inc);
-				//printf("post sem_inc\r\n");
+            case 0x01: //INCREMENT THROTTLE
+                if (interrupt_enable_register & 0x01){
+                    interrupt_enable_register = interrupt_enable_register & 0xFFFE;
+                    OSSemPost(sem_button_inc);
+                }
+                //printf("%d toggle inc\r\n", n);
         		break;
         	case 0x02: //DECREMENT THROTTLE
                 OSSemPost(sem_button_dec);
@@ -63,14 +66,46 @@ void isr_decoder() {
 }
 
 
+/*------------------------------------------------------------------
+ * isr_engine_error --
+ *------------------------------------------------------------------
+ */
+void isr_engine_error() {throttle+=16;
+    OSIntEnter();
+    OSSemPost(sem_error);
+    OSIntExit();
+}
+
+void decoder_error(void *data) {
+    while(TRUE) {
+        OSSemPend(sem_error, WAIT_FOREVER, &err);
+        //printf("pend error\r\n");
+        while(OSSemAccept(sem_error) > 0);
+        X32_LEDS = (X32_LEDS | 0x80);
+        OSTimeDly(5);
+        X32_LEDS = (X32_LEDS & 0x7F);
+    }
+}
+
+
 void update_pwm(void *data) {
 	X32_PWM_PERIOD = (int)data;
-	while(TRUE) {		
+    while(TRUE) {
+        int s = (speed<<4) & 0xff00;
+
 		//PROTECT THROTTLE consistency -MISSING-
 		X32_PWM_WIDTH = throttle;
 		//printf("pwm_period = %d\r\n",X32_PWM_PERIOD);
 		//printf("pwm_width = %d\r\n",X32_PWM_WIDTH);
-		X32_DISPLAY = (speed<<8)|(throttle);
+
+        if (X32_SWITCHES == 0x00){
+            X32_DISPLAY = (s)|(throttle>>4);
+        }else if (X32_SWITCHES == 0x01){
+            X32_DISPLAY = (speed);
+        }else{
+            X32_DISPLAY = (throttle);
+        }
+
 		//printf("speed = %d\r\n",speed);
 		//printf("Throttle: %d\n", throttle);
 		//printf("Speed: %d\n", speed);
@@ -78,50 +113,79 @@ void update_pwm(void *data) {
 	}
 }
 
-void decoder_error(void *data) {
-	while(TRUE) {			
-		OSSemPend(sem_error, WAIT_FOREVER, &err);
-		//printf("pend error\r\n");
-		while(OSSemAccept(sem_error) > 0);
-		//X32_LEDS = (X32_LEDS | 0x80);
-		OSTimeDly(5);
-		X32_LEDS = (X32_LEDS & 0x7F);
-	}
-}
 
 void calculate_speed(void *data) {
-	while(TRUE) {	
-		speed = dec_count;
+    while(TRUE) {
+        speed = X32_DECODER - prev_decoder;
+        prev_decoder = X32_DECODER;
+
 		//speed = speed>>4;
-		dec_count = 0;
-		OSTimeDly(5);
+        X32_DECODER = 0;
+        //dec_count = 0;
+        OSTimeDly(5);
 	}
 }
 
 void button_inc(void *data) {
 	while(TRUE) {
 		OSSemPend(sem_button_inc, WAIT_FOREVER, &err);		
-		OSTimeDly(10);
-		//printf("task button\r\n");
-		while(OSSemAccept(sem_button_inc) > 0);
-		throttle+=10;
-		//printf("increment throttle\r\n");
+        OSTimeDly(2);
+
+        if (X32_BUTTONS & 0x01){
+            throttle += DELTA_MOTOR;
+            OSSemPost(sem_button_inc_auto);
+        }
+
+
+        /*//DAY2
+        //while(OSSemAccept(sem_button_inc) > 0);
+        if (X32_BUTTONS & 0x01){
+            throttle += DELTA_MOTOR;
+        }
+        */
+
+        /*//DAY3 1er intento
+        while (X32_BUTTONS & 0x01){
+            throttle+=16;
+            if ((button_speed & 0x01) == 0){
+                button_speed = button_speed | 0x01;
+                OSTimeDly(25);
+            }else{
+                OSTimeDly(1);
+            }
+        }
+        */
+
+        interrupt_enable_register = interrupt_enable_register | 0x0001;
+
 	}
+}
+
+void button_inc_auto(void *data) {
+    while(TRUE) {
+        OSSemPend(sem_button_inc_auto, WAIT_FOREVER, &err);
+        OSTimeDly(25);
+        while (X32_BUTTONS & 0x01){
+            throttle += DELTA_MOTOR;
+            OSTimeDly(1);
+        }
+
+    }
 }
 
 void button_dec(void *data) {
 	while(TRUE) {
 		OSSemPend(sem_button_dec, WAIT_FOREVER, &err);		
-		OSTimeDly(10);
+        OSTimeDly(15);
 		while(OSSemAccept(sem_button_dec) > 0);
-		throttle-=10;
+        throttle -= DELTA_MOTOR;
 	}
 }
 
 void button_eng(void *data) {
 	while(TRUE) {
 		OSSemPend(sem_button_eng, WAIT_FOREVER, &err);		
-		OSTimeDly(10);
+        OSTimeDly(15);
 		while(OSSemAccept(sem_button_eng) > 0);
 	}
 }
@@ -129,7 +193,7 @@ void button_eng(void *data) {
 void button_res(void *data) {
 	while(TRUE) {
 		OSSemPend(sem_button_res, WAIT_FOREVER, &err);		
-		OSTimeDly(10);
+        OSTimeDly(15);
 		while(OSSemAccept(sem_button_res) > 0);
 		
 		DISABLE_INTERRUPT(INTERRUPT_GLOBAL);
@@ -137,6 +201,7 @@ void button_res(void *data) {
         exit(0);
 	}
 }
+
 
 /*------------------------------------------------------------------
  * main
@@ -148,6 +213,8 @@ void main()
 	throttle = 0;
 	dec_count = 0;
 	speed = 0;
+    prev_decoder = 0;
+    interrupt_enable_register = 0xFF;
 	/* x32 section********************************/
 	
 	/**Define x32 ISR vectors, prioritie,etc.*/
@@ -156,14 +223,23 @@ void main()
 	SET_INTERRUPT_VECTOR(INTERRUPT_BUTTONS, &isr_buttons);
     SET_INTERRUPT_PRIORITY(INTERRUPT_BUTTONS, 10);
 
+    /*
     SET_INTERRUPT_VECTOR(INTERRUPT_ENGINE_A, &isr_decoder);
     SET_INTERRUPT_PRIORITY(INTERRUPT_ENGINE_A, 11);
 
     SET_INTERRUPT_VECTOR(INTERRUPT_ENGINE_B, &isr_decoder);
     SET_INTERRUPT_PRIORITY(INTERRUPT_ENGINE_B, 11);
 
+
     ENABLE_INTERRUPT(INTERRUPT_ENGINE_A);
     ENABLE_INTERRUPT(INTERRUPT_ENGINE_B);
+    */
+
+    SET_INTERRUPT_VECTOR(INTERRUPT_ENGINE_ERROR, &isr_engine_error);
+    SET_INTERRUPT_PRIORITY(INTERRUPT_ENGINE_ERROR, 11);
+
+
+    ENABLE_INTERRUPT(INTERRUPT_ENGINE_ERROR);
     ENABLE_INTERRUPT(INTERRUPT_BUTTONS);
 
 
@@ -176,6 +252,12 @@ void main()
 	sem_button_eng = OSSemCreate(0);
 	sem_button_res = OSSemCreate(0);
 
+
+    sem_button_inc_auto = OSSemCreate(0);
+    sem_button_dec_auto = OSSemCreate(0);
+    sem_button_eng_auto = OSSemCreate(0);
+    sem_button_res_auto = OSSemCreate(0);
+
 	OSTaskCreate(update_pwm, (void *)1024, (void *)stk_update_pwm, PRIO_UPDATE_PWM);
 	OSTaskCreate(decoder_error, (void *)0, (void *)stk_decoder_error, PRIO_DECODER_ERROR);
 	OSTaskCreate(calculate_speed, (void *)0, (void *)stk_calculate_speed, PRIO_CALCULATE_SPEED);
@@ -184,6 +266,8 @@ void main()
 	OSTaskCreate(button_dec, (void *)0, (void *)stk_button_dec, PRIO_BUTTON_DEC);
 	OSTaskCreate(button_eng, (void *)0, (void *)stk_button_eng, PRIO_BUTTON_ENG);
 	OSTaskCreate(button_res, (void *)0, (void *)stk_button_res, PRIO_BUTTON_RES);
+
+    OSTaskCreate(button_inc_auto, (void *)0, (void *)stk_button_inc_auto, PRIO_BUTTON_INC_AUTO);
 
 	/*Initialize rtos*/
 	OSStart();
--- cruise/cc.h
+++ cruise/cc.h
@@ -14,8 +14,11 @@
 #define X32_DECODER				peripherals[PERIPHERAL_ENGINE_DECODED]
 #define X32_PWM_PERIOD 			peripherals[PERIPHERAL_DPC1_PERIOD]
 #define X32_PWM_WIDTH  			peripherals[PERIPHERAL_DPC1_WIDTH]
+#define X32_SWITCHES  			peripherals[PERIPHERAL_SWITCHES]
 
 #define STACK_SIZE 1024
+
+#define DELTA_MOTOR 16
 
 #define PRIO_UPDATE_PWM			10
 #define PRIO_DECODER_ERROR		20
@@ -25,6 +28,11 @@
 #define PRIO_BUTTON_DEC			33
 #define PRIO_BUTTON_ENG			34
 #define PRIO_BUTTON_RES			35
+
+#define PRIO_BUTTON_INC_AUTO	36
+#define PRIO_BUTTON_DEC_AUTO	37
+#define PRIO_BUTTON_ENG_AUTO	38
+#define PRIO_BUTTON_RES_AUTO	39
 
  #include "stdint.h"
 
@@ -37,17 +45,27 @@ static unsigned int stk_button_eng[STACK_SIZE];
 static unsigned int stk_button_inc[STACK_SIZE];
 static unsigned int stk_button_dec[STACK_SIZE];
 
+static unsigned int stk_button_res_auto[STACK_SIZE];
+static unsigned int stk_button_eng_auto[STACK_SIZE];
+static unsigned int stk_button_inc_auto[STACK_SIZE];
+static unsigned int stk_button_dec_auto[STACK_SIZE];
+
 unsigned char err;
 uint16_t throttle;
-uint16_t speed;
+uint16_t speed, prev_decoder;
 int dec_count;
 char new_a, new_b;
 char state_a, state_b;
+char interrupt_enable_register;
 
 OS_EVENT *sem_error;
 OS_EVENT *sem_button_inc;
 OS_EVENT *sem_button_dec;
 OS_EVENT *sem_button_eng;
 OS_EVENT *sem_button_res;
+OS_EVENT *sem_button_inc_auto;
+OS_EVENT *sem_button_dec_auto;
+OS_EVENT *sem_button_eng_auto;
+OS_EVENT *sem_button_res_auto;
 
  #endif      /* __IN2305_CC__ */
